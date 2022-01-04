df8d30c4f4e190507cbd26c8dc87826b8c5b2973
thunderent <robert.nechitelea@gmail.com>
2022-01-04 17:12:58 +0200

    first section creation

--- src/components/Index.styles.tsx
+++ src/components/Index.styles.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import styled, {createGlobalStyle} from 'styled-components';
 
 
+interface ContactProps {
+    animTriggered : boolean
+}
+
 /* Header */
 const HeaderContainer = styled.nav`
     width : 100%;
     height : 40px;
     margin-top : 11px;
+    margin-bottom : 8px;
     box-sizing : border-box;
     display : flex;
     flex-direction : row;
@@ -51,7 +56,108 @@ const NavLine = styled.div`
     width : 0px;
 
     transition : width 0.2s ease-in, background 0.4s ease-in;
+`;
 
+const SayHiButton = styled.div<ContactProps>`
+    opacity : ${props => props.animTriggered ? 1 : 0};
+    position : fixed;
+    top : 14px;
+    left : 19px;
+    color : ${props => props.theme.main};
+    font-size : 21px;
+    font-weight : 500;
+    cursor : pointer;
+    user-select : none;
+
+    transition : color 0.1s ease-in-out, opacity 0.5s ease-in-out;
+   
+
+    svg{
+        filter: drop-shadow( 3px 2px 0px ${props => props.theme.secondary});
+        margin-right : 4px;
+        transition : filter 0.1s ease-in-out;
+    }
+
+    &:hover{
+        svg{
+            filter: drop-shadow( 3px 2px 0px ${props => props.theme.main});
+            color : ${props => props.theme.secondary};
+        }
+    }
+
+    &:hover{
+        color : ${props => props.theme.secondary};
+    }
+ 
+`;
+/* End Header */
+
+/* Section */
+const Section = styled.div`
+    position : relative;
+    display : flex;
+    flex-direction : row;
+    align-items : center;
+    justify-content : space-between;
+`;
+const Rectangle = styled.div`
+    width : 70%;
+    background : #19223C;
+    height : 300px;
+    position : absolute;
+    z-index : -1;
+    top: 50%;
+    transform: translateY(-50%);
+    right : 0;
+`;
+const Title = styled.h1`
+    color : ${props => props.theme.text.main};
+    font-size : 56px;
+    font-weight : 400;
+    margin : 0;
+`;
+const Description = styled.h4`
+    color : ${props => props.theme.text.main};
+    font-size : 22px;
+    font-weight : 300;
+`;
+
+const ContactMeButton = styled.div<ContactProps>`
+    height : ${props => props.animTriggered ? '2px' : '46px'};
+    overflow : hidden;
+    width : 200px;
+    text-align : center;
+    line-height : 46px;
+    color : ${props => props.theme.text.main};
+    border-radius : 9px;
+    background : ${props => props.theme.main};
+    box-shadow: ${props => props.theme.main} 0px 0px 0px 0px inset, #222E4F 6px 7px 0px -3px, ${props => props.theme.secondary} 6px 7px;
+    cursor : pointer;
+    user-select : none;
+
+    transition : background 0.1s ease-in-out, box-shadow 0.1s ease-in-out, height 0.5s ease-in-out;
+
+
+    &:hover{
+        background : ${props => props.theme.secondary};
+        box-shadow: ${props => props.theme.secondary} 0px 0px 0px 0px inset, #222E4F 6px 7px 0px -3px, ${props => props.theme.main} 6px 7px;
+    }
+
+    &:active{
+        box-shadow: #19223C 0px 0px 0px 0px inset, #222E4F 2px 4px 0px -3px, #19223C 2px 4px; 
+    }
+`;
+
+const AnimatedSvg = styled.svg<ContactProps>`
+    display : inline-block;
+    margin : auto;
+    transform : ${props => props.animTriggered ? 'rotate(90deg) scale(1,0.8)' : null};
+    transition : transform 0.5s ease-in;
+
+    text{
+        opacity : ${props => props.animTriggered ? '0' : '1'};
+        transition : opacity 0.1s ease-in;
+    }
 `;
 
 const GlobalStyle = createGlobalStyle`
@@ -83,7 +189,14 @@ export {
     NavNumber,
     HeaderContainer,
     GlobalStyle,
-    Container
+    Container,
+    Section,
+    Rectangle,
+    Title,
+    Description,
+    ContactMeButton,
+    SayHiButton,
+    AnimatedSvg
 }
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import Header from "../components/Header";
 import * as SC from "../components/Index.styles";
 import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCommentAlt } from '@fortawesome/free-solid-svg-icons'
 
 type Theme = {
     main : string,
@@ -24,15 +27,79 @@ const mainTheme : Theme = {
     filler : '#5F5F5F'
 }
 
-const IndexPage = () => (
-  <ThemeProvider theme={mainTheme}> 
-    <SC.Container>
-        <Header/>
-        <SC.GlobalStyle/>
-        <Seo title="Robert NC" />
-        <h2>The brown fox jumped over the fence</h2>
-    </SC.Container>
-  </ThemeProvider>
-)
+const IndexPage = () => {
+    const [offset, setOffset] = useState(0);
+  
 
-export default IndexPage
+    useEffect(() => {
+        const handleScroll = () => {
+            setOffset(window.pageYOffset);
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+
+    return(
+        <ThemeProvider theme={mainTheme}> 
+            <SC.Container>
+                <Header/>
+                <SC.GlobalStyle/>
+                <Seo title="Robert NC" />
+             
+                <SC.Section>
+                    <SC.SayHiButton animTriggered={offset > 500}>
+                        <FontAwesomeIcon icon={faCommentAlt}/> Say Hi!
+                    </SC.SayHiButton>
+                  
+                
+                    <div style={{maxWidth:'38%'}}>
+                        <SC.Title>Hello, my</SC.Title>
+                        <SC.Title>name is <strong style={{color : mainTheme.main}}>Robert</strong>.</SC.Title>
+                        <SC.Description>I am a <strong>full-stack software engineer</strong> living in Romania. I specialize in crafting <strong>dynamic, feature rich Web Experiences.</strong></SC.Description>
+                        <SC.ContactMeButton animTriggered={offset > 500}>GET IN TOUCH</SC.ContactMeButton>
+                    </div>
+                    <div style={{width : '62%', display : 'flex', justifyContent : 'center'}}>
+                        <StaticImage
+                            src="../images/laptop.png"
+                            width={700}
+                            quality={95}
+                            formats={["auto", "webp", "avif"]}
+                            alt="A Gatsby astronaut"
+                        />
+                    </div>
+                    <SC.Rectangle/>
+                </SC.Section>
+                <SC.Section>
+                    <SC.AnimatedSvg animTriggered={offset > 650} width="300" height="220">
+                        <line x1="150" y1="0" x2="150" y2="220" style={{stroke : mainTheme.secondary , strokeWidth : 2}}/>
+                        <text x="160" y="35" style={{fill : mainTheme.secondary}}>
+                            Scroll
+                            <animate attributeName="y" values="35;50;35" dur="2s" repeatCount="indefinite" />
+                        </text>
+                    </SC.AnimatedSvg>
+                </SC.Section>
+                <SC.Section>
+                    <div style={{maxWidth:'38%'}}>
+                        <SC.Title>Hello, my</SC.Title>
+                        <SC.Title>name is <strong style={{color : mainTheme.main}}>Robert</strong>.</SC.Title>
+                        <SC.Description>I am a <strong>full-stack software engineer</strong> living in Romania. I specialize in crafting <strong>dynamic, feature rich Web Experiences.</strong></SC.Description>
+                        <SC.ContactMeButton>GET IN TOUCH</SC.ContactMeButton>
+                    </div>
+                    <div style={{width : '62%', display : 'flex', justifyContent : 'center'}}>
+                        <StaticImage
+                            src="../images/laptop.png"
+                            width={700}
+                            quality={95}
+                            formats={["auto", "webp", "avif"]}
+                            alt="A Gatsby astronaut"
+                        />
+                    </div>
+                    <SC.Rectangle/>
+                </SC.Section>
+            </SC.Container>
+        </ThemeProvider>
+    )
+}
+
+export default IndexPage;
